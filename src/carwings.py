aa47c4c8ac0a12debdb239d4f2f50daf6294bd77
Tom Hill <tom@clockworkcubed.com>
2018-01-01 11:13:58 +0000

    Add some logging

--- src/carwings.py
+++ src/carwings.py
@@ -9,11 +9,13 @@ from pycarwings2.responses import CarwingsBatteryStatusResponse
 class CarwingsStatus:
     def __init__(self, username, password):
         self._leaf = None
+        self.log = logging.getLogger()
+        self.log.setLevel(logging.INFO)
         self.session = pycarwings2.Session(username, password, 'NE')
 
     def _login(self):
         if not self.session.logged_in:
-            logging.info('Logging in to carwings')
+            self.log.info('Logging in to carwings')
             try:
                 result = self.session.connect()
             except pycarwings2.pycarwings2.CarwingsError:
@@ -21,36 +23,36 @@ class CarwingsStatus:
             return result
 
     def request_update(self):
-        logging.info('Requesting update from Nissan')
+        self.log.info('Requesting update from Nissan')
         result_key = self.leaf().request_update()
         return {"resultKey": result_key}
 
     def is_update_available(self, event):
         rk = event['resultKey']
-        logging.info('Checking if update available')
-        logging.info('Input event was: ')
-        logging.info(event)
+        self.log.info('Checking if update available')
+        self.log.info('Input event was: ')
+        self.log.info(event)
         ret = isinstance(self.leaf().get_status_from_update(rk),
                          CarwingsBatteryStatusResponse)
         result = {'updateAvailable': ret}
         if ret:
             result.update(self._collate_data())
-        logging.info('Result was: ')
-        logging.info(result)
+        self.log.info('Result was: ')
+        self.log.info(result)
         return result
     
     def leaf(self):
         if self._leaf:
             return self._leaf
         self._login()
-        logging.info('Getting car information')
+        self.log.info('Getting car information')
         return self.session.get_leaf()
         
     def _collate_data(self):
         dicts = []
         res = {}
         month = date.today().strftime('%Y%m')
-        logging.info('Collecting date from update')
+        self.log.info('Collecting date from update')
         dicts.append(self.leaf().get_driving_analysis())
         dicts.append(self.leaf().get_latest_battery_status())
         dicts.append(self.leaf().get_electric_rate_simulation(month))
@@ -58,8 +60,8 @@ class CarwingsStatus:
         for d in dicts:
             if d:
                 res.update(d)
-        logging.info('Collected data:')
-        logging.info(res)
+        self.log.info('Collected data:')
+        self.log.info(res)
         return res
 
 
